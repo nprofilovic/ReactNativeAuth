4de79bc3065c695ad6a1944b63d832c65bf9ef21
nprofilovic <nprofilovic@gmail.com>
2019-07-21 13:35:13 +0200

    Make Input and spinner components

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,34 +1,58 @@
 import React from 'react';
-import { TextInput } from 'react-native';
+import { Text, StyleSheet } from 'react-native';
+import firebase from 'firebase';
 import Card from './common/Card';
 import CardSection from './common/CardSection';
 import Button from './common/Button';
 import Input from './common/Input';
-
+import Spinner from './common/Spinner';
 
 class LoginForm extends React.Component{
     state = {
-        text: ''
+        email: '',
+        password: '',
+        error: ''
+    }
+
+    onButtonPress() {
+        const { email, password } = this.state;
+
+        this.setState({ error: '' });
+
+        firebase.auth().signInWithEmailAndPassword(email, password) 
+            .catch(() => {
+                firebase.auth().createUserWithEmailAndPassword(email, password)
+                    .catch(() => {
+                        this.setState({error: 'Authentication Failed.'})
+                    })
+            })
     }
     render(){
         return(
             <Card>
                 <CardSection>
                     <Input 
-                        onChangeText = {text => this.setState({text: text})}
-                        value = {this.state.text}
+                        
+                        placeholder = "user@gmail.com"
+                        onChangeText = {email => this.setState({email})}
+                        value = {this.state.email}
                         label={'Email'}
                     />
                 </CardSection>
                 <CardSection>
                     <Input 
-                        onChangeText = {text => this.setState({text: text})}
-                        value = {this.state.text}
+                        secure = {true}
+                        placeholder= "password"
+                        onChangeText = {password => this.setState({password})}
+                        value = {this.state.password}
                         label={'Password'}
                     />
                 </CardSection>
+                <Text style={styles.error}>
+                    {this.state.error}
+                </Text>
                 <CardSection>
-                    <Button>
+                    <Button onPress={this.onButtonPress.bind(this)}>
                         Log in
                     </Button>
                 </CardSection>
@@ -36,5 +60,14 @@ class LoginForm extends React.Component{
         )
     }
 }
+
+const styles = StyleSheet.create({
+    error: {
+        fontSize: 20,
+        alignSelf:'center',
+        color: 'red'
+    }
+})
+
 
 export default LoginForm;
--- src/components/common/Input.js
+++ src/components/common/Input.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { TextInput, View, Text } from 'react-native';
+import { TextInput, View, Text, StyleSheet } from 'react-native';
 
-const Input = ({label, onChangeText, value}) => {
+const Input = ({label, onChangeText, value, placeholder, secure}) => {
+    const {container, input, labelStyle} = styles
     return(
-        <View>
-            <Text>{label}</Text>
+        <View style={container}>
+            <Text style={labelStyle}>{label}</Text>
             <TextInput 
-                style={{height:20, width:100}} 
+                secureTextEntry = {secure}
+                placeholder={placeholder}
+                autoCorrect={false}
+                style={input} 
                 onChangeText={onChangeText}
                 value={value}
             />
@@ -14,5 +18,25 @@ const Input = ({label, onChangeText, value}) => {
     );
 };
 
-
+const styles = StyleSheet.create({
+    input: {
+        color: '#000',
+        paddingRight: 5,
+        paddingLeft: 5,
+        fontSize:18,
+        lineHeight: 23,
+        flex: 2
+    },
+    labelStyle: {
+        fontSize: 18,
+        paddingLeft: 20,
+        flex: 1
+    },
+    container: {
+        height: 40,
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center'
+    }
+})
 export default Input;
